05aeafc1d37fda8996ff31e04e06705e1864e354
ChopinGuo <gshaop1@lsu.edu>
2021-04-17 12:51:34 -0500

    Add in-place version of the iterative policy evaluation and compare its running time with that of the two-array version

--- createGridWorld.m
+++ createGridWorld.m
@@ -15,13 +15,13 @@ gridCols = 5;
 % % All actions have the same probability.
 % actions = [1, 2, 3, 4, 5];
 
-% Five possible actions 
+% Four possible actions 
 % North = 1, South = 2, East = 3, West = 4
 % All actions have the same probability.
 actions = [1, 2, 3, 4];
 
 % initialState = [2, 1];       % The agent begins from cell [2,1].
-terminalState = [5, 5];      % The terminal state at cell [5,5].
+terminalState = [5, 5];        % The terminal state at cell [5,5].
 
 % The environment contains a special jump from cell [2,4] to cell [4,4]
 % with a reward of +5.
@@ -48,9 +48,11 @@ gridValues(obstacle3(1),obstacle3(2)) = -inf;
 gridValues(obstacle4(1),obstacle4(2)) = -inf;
 
 %%
-theta = 0.01;    % The paprameter determing the accuracy of extimation
+theta = 0.0001;    % The paprameter determing the accuracy of extimation
 delta = theta + 1;
 
+% two-array version
+tic
 while delta > theta || delta == theta
     delta = 0;
     gridValues_old = gridValues;
@@ -65,6 +67,25 @@ while delta > theta || delta == theta
         end
     end
 end
+toc
+
+% % in-place version
+% tic
+% while delta > theta || delta == theta
+%     delta = 0;
+% %     gridValues_old = gridValues;
+%     for i=1:gridRows
+%         for j=1:gridCols
+%             state = [i,j];
+%             v = gridValues(state(1),state(2));
+%             gridValues(state(1),state(2)) = value(gridValues,state);
+%             error = abs(v-gridValues(state(1),state(2)));
+%             delta = max(delta, error);
+%             gridValues
+%         end
+%     end
+% end
+% toc
 
 
 
