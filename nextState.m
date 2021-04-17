3b2fd72f7c6ee9c7d3a7e466251eae402c046fcc
ChopinGuo <gshaop1@lsu.edu>
2021-04-16 23:15:20 -0500

    Homework 9 version 1

---
+++ nextState.m
@@ -0,0 +1,24 @@
+function ns = nextState(state, action)
+specialState = [2, 4];
+specialJump = [4, 4];
+ns = zeros(1, 2);
+
+switch action
+    case 1
+        ns(1) = state(1) - 1;
+        ns(2) = state(2);
+    case 2 
+        ns(1) = state(1) + 1;
+        ns(2) = state(2);
+    case 3 
+        ns(1) = state(1);
+        ns(2) = state(2) + 1;
+    case 4
+        if isequal(state, specialState)
+            ns = specialJump;
+        else
+            ns(1) = state(1);
+            ns(2) = state(2) - 1;
+        end
+end
+end
---
+++ value.m
@@ -0,0 +1,19 @@
+function gridValue = value(gridValues_old, state)
+
+actions = possibleActions(state);
+reward = -1;
+gamma = 1;
+
+if isempty(actions)
+    gridValue = gridValues_old(state(1),state(2));
+else
+    gridValue = 0;
+    numActions = size(actions,2);
+    prob = 1/numActions;            % All actions have the same probability
+    for i=1:numActions
+        action = actions(i);
+        ns = nextState(state, action);
+        gridValue = gridValue ...
+            + prob*(reward + gamma*gridValues_old(ns(1),ns(2)));
+    end
+end
